e7d7166f966bf7a9c41bd0b2f133b8d27daffaf9
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 05:51:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 def key_for_min_value(hash)
-    lowest_key = nil 
-    lowest_value = nil 
+    lowest_key = nil
+    lowest_value = nil
     name_hash.each do |k, v|
       if lowest_value == nil || v < lowest_value
-        lowest_value = v 
-        lowest_key = k 
-      end 
-    end 
+        lowest_value = v
+        lowest_key = k
+      end
+    end
     lowest_key
-  end 
+  end
