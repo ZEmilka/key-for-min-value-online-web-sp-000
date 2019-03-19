1f157a6fd3f1dd9906f1fd86d90382f9f5b8a4ed
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 05:46:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,15 @@
 def key_for_min_value(hash)
-    hash.each {|k,v| v}
-    return hash[0][0]
+  if hash = {}
+    return nil
+  else
+      min_value = hash.first[1]
+      min_key = hash.first[0]
+      hash.each do |key, value|
+        if value < min_value
+          min_value = value
+          min_key = key
+        end
+      end
+      min_key
+  end 
 end
