a1b9fd67e519b2b99b80f79b8845ee3dd8ba90a8
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 04:53:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
   lower=name_hash.find{|l| l[:value]<name_hash[:value]}
