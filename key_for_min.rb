47de0b83a34990a979f8a7688cccfa3e5ae80aee
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 05:47:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,6 @@ def key_for_min_value(hash)
   if hash = {}
     return nil
   else
-      min_value = hash.first[1]
-      min_key = hash.first[0]
       hash.each do |key, value|
         if value < min_value
           min_value = value
