c9f7fb22d20816f9814fbcc361699afe1cd414e3
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 05:04:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@ def key_for_min_value(hash)
   if hash == {}
        return nil
   else
-       test = test.sort_by {|k, v| v}
-       test[0][0]
+       hash = hash.sort_by {|k, v| v}
+       hash[0][0]
    end
 end
